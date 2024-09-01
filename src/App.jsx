6d7dd16dc68eb43a12faaaa0613c0c9dead846a7
chaki <sherkooch1@gmail.com>
2024-09-02 01:33:50 +0430

    finally got pushed

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import CoreConcept from './components/CoreConcept.jsx';
+import Examples from './components/Examples.jsx';
+
+function App() {
+
+  return (
+    <div>
+      <main>
+        <Examples />
+        <CoreConcept />
+      </main>
+    </div>
+  );
+}
+
+export default App;
